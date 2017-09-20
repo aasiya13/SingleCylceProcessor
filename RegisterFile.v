47059773f93258cc69181f194d8ae733a5b2dd83
aasiya13 <sitharapitiyage@gmail.com>
2017-09-20 12:18:07 +0530

    finish Register File

--- RegisterFile.v
+++ RegisterFile.v
@@ -6,22 +6,26 @@ module RegisterFile;
 	reg [7:0]IN;
 	
 	reg_file REGISTER(IN,OUT1,OUT2,clk,RESET,INaddr,OUT1addr,OUT2addr);
-	
-	always
-		#5 clk = ~clk;
 		
-	initial
-		#100 $finish;
+	//always
+		//#5 clk = ~clk;
 		
-	initial begin
+	//initial
+		//#200 $finish;
+		
+	initial begin  
+	OUT1addr = 3'b001; OUT2addr = 3'b000; INaddr = 3'b101; RESET = 1'b1; IN = 8'b1111_1111; clk = 1'b1; 
+	#5 $display("Out 1 = %b Out 2 = %b OUT1addr = %b OUT2addr = %b IN = %b clock = %b \n",OUT1, OUT2, OUT1addr,OUT2addr,IN,clk);
 	
-	// test the clear input
-	#5 OUT1addr = 3'b010; OUT2addr = 3'b011; INaddr = 3'b101; RESET = 1'b1; clk = 1'b1; IN = 8'b1001_0111 ; 
-	#5	$display("Out 1 = %b Out 2 = %b OUT1addr = %b OUT2addr = %b ",OUT1, OUT2, OUT1addr,OUT2addr);
-	
-	#5 OUT1addr = 3'b111; OUT2addr = 3'b000; INaddr = 3'b101; RESET = 1'b1; clk = 1'b1; IN = 8'b1001_0111 ; 
-	#5	$display("Out 1 = %b Out 2 = %b OUT1addr = %b OUT2addr = %b ",OUT1, OUT2, OUT1addr,OUT2addr);
-	end 
+	OUT1addr = 3'b001; OUT2addr = 3'b000; INaddr = 3'b101; RESET = 1'b1; IN = 8'b1111_0111; clk = 1'b0; 
+	#10 $display("Out 1 = %b Out 2 = %b OUT1addr = %b OUT2addr = %b IN = %b clock = %b \n",OUT1, OUT2, OUT1addr,OUT2addr,IN,clk);
+		
+	OUT1addr = 3'b001; OUT2addr = 3'b000; INaddr = 3'b101; RESET = 1'b1; IN = 8'b0000_0111; clk = 1'b1;
+	#15 $display("Out 1 = %b Out 2 = %b OUT1addr = %b OUT2addr = %b IN = %b clock = %b \n",OUT1, OUT2, OUT1addr,OUT2addr,IN,clk);
+		
+	OUT1addr = 3'b001; OUT2addr = 3'b000; INaddr = 3'b001; RESET = 1'b1; IN = 8'b1001_0101; clk = 1'b0 ; 
+	#20	$display("Out 1 = %b Out 2 = %b OUT1addr = %b OUT2addr = %b IN = %b clock = %b \n",OUT1, OUT2, OUT1addr,OUT2addr,IN,clk);
+	end
 
 endmodule
 
@@ -34,17 +38,27 @@ module reg_file(IN,OUT1,OUT2,clk,RESET,INaddr,OUT1addr,OUT2addr);
 	input RESET;
 	
 	output [7:0]OUT1,OUT2;
-	reg [7:0]temp;
+	reg [7:0]temp,temp1;
 	
 	reg [7:0]registers[7:0]; 
 	
 	always @(negedge clk)begin
-		registers[0] = 8'b1000_1000;
-		temp = registers[0];
-		registers[3'b001] = 8'b1000_1000;
+		registers[INaddr] = IN; 	// in negetive edges value in IN register stores registerFile at the location specify by the INaddr
+	//	temp = registers[INaddr];	
+		$display("negetive edge");
+		$display("Value of INaddr : %b ", registers[INaddr]);
+	end
 	
+	always @(posedge clk)begin
+		registers[OUT2addr] = 8'b1000_1000; // store value at the location OUT2addr
+		temp = registers[OUT2addr];
+		registers[OUT1addr] = 8'b1111_1000; // store value at the location OUT1addr
+		temp1 = registers[OUT1addr]; 
+		$display("positive edge");
+		$display("Value of INaddr : %b ", registers[INaddr]);
 	end
 
 	assign OUT1 = temp;
+	assign OUT2 = temp1;
 
 endmodule 
