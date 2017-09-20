f8e9f5b3fde32000dea86bd96d7be1b4b48cb492
aasiya13 <sitharapitiyage@gmail.com>
2017-09-20 16:02:16 +0530

    fixes some issues on test bench of Register file

--- RegisterFile.v
+++ RegisterFile.v
@@ -7,24 +7,26 @@ module RegisterFile;
 	
 	reg_file REGISTER(IN,OUT1,OUT2,clk,RESET,INaddr,OUT1addr,OUT2addr);
 		
-	//always
-		//#5 clk = ~clk;
+	always
+		#5 clk = ~clk;
 		
-	//initial
-		//#200 $finish;
+	initial
+		#20 $finish;
 		
 	initial begin  
-	OUT1addr = 3'b001; OUT2addr = 3'b000; INaddr = 3'b101; RESET = 1'b1; IN = 8'b1111_1111; clk = 1'b1; 
-	#5 $display("Out 1 = %b Out 2 = %b OUT1addr = %b OUT2addr = %b IN = %b clock = %b \n",OUT1, OUT2, OUT1addr,OUT2addr,IN,clk);
+	clk = 1'b1;
+//	 $monitor("time = %d \t clock 1= %b ",$time,clk);
+	#5 OUT1addr = 3'b001; OUT2addr = 3'b000; INaddr = 3'b101; RESET = 1'b1; IN = 8'b1111_1111;  
+	 $monitor("Out 1 = %b Out 2 = %b OUT1addr = %b OUT2addr = %b IN = %b clock = %b \n",OUT1, OUT2, OUT1addr,OUT2addr,IN,clk);
 	
-	OUT1addr = 3'b001; OUT2addr = 3'b000; INaddr = 3'b101; RESET = 1'b1; IN = 8'b1111_0111; clk = 1'b0; 
-	#10 $display("Out 1 = %b Out 2 = %b OUT1addr = %b OUT2addr = %b IN = %b clock = %b \n",OUT1, OUT2, OUT1addr,OUT2addr,IN,clk);
+	OUT1addr = 3'b001; OUT2addr = 3'b000; INaddr = 3'b101; RESET = 1'b1; IN = 8'b1111_0111; 
+	#10 $monitor("Out 1 = %b Out 2 = %b OUT1addr = %b OUT2addr = %b IN = %b clock = %b \n",OUT1, OUT2, OUT1addr,OUT2addr,IN,clk);
 		
-	OUT1addr = 3'b001; OUT2addr = 3'b000; INaddr = 3'b101; RESET = 1'b1; IN = 8'b0000_0111; clk = 1'b1;
-	#15 $display("Out 1 = %b Out 2 = %b OUT1addr = %b OUT2addr = %b IN = %b clock = %b \n",OUT1, OUT2, OUT1addr,OUT2addr,IN,clk);
+	OUT1addr = 3'b001; OUT2addr = 3'b000; INaddr = 3'b101; RESET = 1'b1; IN = 8'b0000_0111; 
+	#15 $monitor("Out 1 = %b Out 2 = %b OUT1addr = %b OUT2addr = %b IN = %b clock = %b \n",OUT1, OUT2, OUT1addr,OUT2addr,IN,clk);
 		
-	OUT1addr = 3'b001; OUT2addr = 3'b000; INaddr = 3'b001; RESET = 1'b1; IN = 8'b1001_0101; clk = 1'b0 ; 
-	#20	$display("Out 1 = %b Out 2 = %b OUT1addr = %b OUT2addr = %b IN = %b clock = %b \n",OUT1, OUT2, OUT1addr,OUT2addr,IN,clk);
+	OUT1addr = 3'b001; OUT2addr = 3'b000; INaddr = 3'b001; RESET = 1'b1; IN = 8'b1001_0101;  
+	#20	$monitor("Out 1 = %b Out 2 = %b OUT1addr = %b OUT2addr = %b IN = %b clock = %b \n",OUT1, OUT2, OUT1addr,OUT2addr,IN,clk);
 	end
 
 endmodule
